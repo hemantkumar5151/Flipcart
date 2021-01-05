7b83bbb34fe46f863c1933a3993cff1fd46f3f24
Hemant <hemantsingh5151@gmail.com>
2021-01-05 14:16:35 +0530

    HomePage.js  page is added

--- frontend/src/pages/home/HomePage.js
+++ frontend/src/pages/home/HomePage.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import CategoryList from '../../components/category/CategoryList';
 
+import Jumbotron from '../../components/Jumbotron';
+import ProductArrival from '../../components/product/ArrivalProduct';
+import BestSeller from '../../components/product/BestSeller';
+import SubList from '../../components/sub/SubList';
 const HomePage = () => {
-    return (
-        <div>
-            Home Page
-        </div>
-    )
+    return <>
+            <div className="jumbotron font-weight bold text-center h1 ">
+                <Jumbotron text={['New Arrival', 'Latest Product', 'Best Product']} />
+            </div>
+            
+            <ProductArrival />
+            
+            <BestSeller /> 
+
+            <CategoryList />
+
+            <SubList />
+        </>
 }
 
 export default HomePage;
